150494070ecac98da7bc6c84f0f8208d3198e6ec
Everton Christmann <evertonluiz00@hotmail.com>
2021-10-12 21:54:12 -0300

    Initial commit

---
+++ TaskManager/TaskManager.Api/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpPost]
+        public IActionResult FazerLogin([FromBody] LoginRequestDto request)
+        {
+            try
+            {
+                if (request == null || string.IsNullOrEmpty(request.Login.Trim()) || string.IsNullOrEmpty(request.Senha.Trim()))
+                {
+                    return BadRequest(new ErrorResponseDto()
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Erro = "Parâmetros de entrada inválidos."
+                    });
+                }
+
+                return Ok("Usuário autenticado.");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Erro ao efetuar login: {e.Message}");
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorResponseDto() { 
+                    Status = StatusCodes.Status500InternalServerError,
+                    Erro = $"Erro ao efetuar login, tente novamente."
+                });
+            }
+        }
+    }
+}
---
+++ TaskManager/TaskManager.Api/Dtos/ErrorResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Api
+{
+    public class ErrorResponseDto
+    {
+        public int Status { get; set; }
+        public string Erro { get; set; }
+    }
+}
